df4d7be646554bbf7bd8a962da042b2fa23b6541
Swapnil Agarwal <a.swapnil@iitg.ernet.in>
2013-10-25 23:37:44 +0530

    Fixes #1
    v1.0.1

--- CodeChef Submit Helper/add.js
+++ CodeChef Submit Helper/add.js
@@ -1,10 +1,12 @@
 function addSubmitButton () {
-    var ul = document.getElementsByClassName("button-list");
-    if(ul[0].childNodes.length==5){
-        var content = ul[0].innerHTML;
-        var pathArray = window.location.pathname.split( '/' );
-        var problemId = pathArray[pathArray.length-1];
-        ul[0].innerHTML = "<li><a href=\"/submit/" + problemId + "\">Submit</a></li>" + content;
+    var pathArray = window.location.pathname.split( '/' );
+    if ( pathArray[pathArray.length-2] == "problems" ) {
+        var ul = document.getElementsByClassName("button-list");
+        if(ul[0].childNodes.length==5){
+            var content = ul[0].innerHTML;
+            var problemId = pathArray[pathArray.length-1];
+            ul[0].innerHTML = "<li><a href=\"/submit/" + problemId + "\">Submit</a></li>" + content;
+        }
     }
 }
 addSubmitButton();
